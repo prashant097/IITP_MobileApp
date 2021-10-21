99d959120fa5d9d924d0515347d7eeadf7056b93
prashant097 <prashantkumarsahni@gmail.com>
2021-10-21 16:35:44 +0530

    21st Oct Eve

--- test/MyTestApp2/mainFolder/RTT.js
+++ test/MyTestApp2/mainFolder/RTT.js
@@ -34,36 +34,40 @@ export default class RTT extends Component {
 
     this.state = {
       // ipAddress: '192.168.1.102', // Nokia Mobile Hotspot IP address
+      // ipAddress: '192.168.1.101', // Nokia Mobile Hotspot ETHERNET IP address
       // ipAddress: '192.168.42.1', // USB Tethering IP address
       // ipAddress: '27.59.136.8', // Dongle Airtel IP address
       //ipAddress: '27.59.136.145', // Dongle Airtel IP address
       // ipAddress: '106.195.71.43', //  Airtel Nokia Phone IP address
-      ipAddress: '106.195.65.41', //  Airtel Nokia Phone IP address
-      ms: '',
-      // ms: null,
+      // ipAddress: '106.195.65.41', //  Airtel Nokia Phone IP address
+      // ipAddress: '106.195.66.163', //  Airtel Nokia Phone IP address
+      // ms: '',
+      ms: null,
     };
   }
   onPressButton = async () => {
     const option = { timeout: 1000 };
     console.log("Check1");
-    // var ips = ['106.195.65.41','27.59.136.8', '192.168.1.102','192.168.1.100'];
-    // for (const item of ips) {
+    var ips = ['106.195.65.41','27.59.136.8', '192.168.1.102','192.168.1.101','106.195.70.195'];
+    for (const item of ips) {
     try {
-      ms = await Ping.start('ipAddress', option);
+      // const ms = await Ping.start(this.state.ipAddress, option);
+      const ms = await Ping.start(item, option);
       // this.state.ms = await Ping.start('ipAddress', option);
 
       // const ms = await Ping.start(this.state.ipAddress, { timeout: 1000 });
       console.log("ms :" + ms);
-      // this.setState({ ms });
+      this.setState({ ms });
     } catch (error) {
       console.log("ERROR:" + error.code, error.message);
     }
 
-    this.setState({ ms });
+    // this.setState({ ms });
 
     const result = await Ping.getTrafficStats();
-    console.log("ipAddress: "+this.state.ipAddress+" result::"+JSON.stringify(result));
-  // }
+    // console.log("ipAddress: "+this.state.ipAddress+" result::"+JSON.stringify(result));
+    console.log("item: "+item+" result::"+JSON.stringify(result));
+  }
     // console.log("result: "+result);
   };
 
